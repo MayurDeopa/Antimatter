cfcffd823898fb0e2c8ab2342627d269000ec064
MayurDeopa <frostkickerzvashi@gmail.com>
2022-05-12 19:12:40 +0530

    added a carousel

--- components/Display/ImageSlider.js
+++ components/Display/ImageSlider.js
@@ -1,26 +1,22 @@
 import styles from '../../styles/misc.module.css'
-
-
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import Image from 'next/image';
 
 const ImageSlider =({children})=>{
     return (
-        <div className={styles.image_slider}>
-            <div className={styles.image_container}>
-                <Image 
-                    src={'https://i.ibb.co/yfzmWt7/1298817-D-6-C08-4685-9-C04-B744-DF55-A64-E-2.jpg'}
-                    height={1200}
-                    width={1700}
-                />
-            </div>
-            <div className={styles.image_container}>
-                <Image 
-                    src={'https://i.ibb.co/yfzmWt7/1298817-D-6-C08-4685-9-C04-B744-DF55-A64-E-2.jpg'}
-                    height={1200}
-                    width={1700}
-                />
-            </div>
-        </div>
+        <Carousel
+            autoPlay={true}
+            infiniteLoop
+            interval={5000}
+            showArrows={false}
+            showStatus={false}
+            showIndicators={false}
+            swipeable={true}
+            
+        >
+            {children}
+        </Carousel>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import PageWrapper from '../components/PageWrapper'
 import {BiRightArrowAlt} from 'react-icons/bi'
 import styles from '../styles/Home.module.css'
+import ImageSlider from '../components/Display/ImageSlider'
 
 export default function Home() {
   
@@ -12,6 +13,7 @@ export default function Home() {
       <Head >
         <title>Anti-Matter</title>
       </Head>
+      <ImageSlider>
       <div 
         className={styles.home_image_container}
         style={{
@@ -28,6 +30,23 @@ export default function Home() {
               </Link>
           </div>
       </div>
+      <div 
+        className={styles.home_image_container}
+        style={{
+          backgroundImage:'url("https://i.ibb.co/yfzmWt7/1298817-D-6-C08-4685-9-C04-B744-DF55-A64-E-2.jpg")'
+        }}>
+          <div className={styles.home_image_text}>
+            <h1>Gfagfagfag</h1>
+            <h2>Some random quote</h2>
+            <Link href={'/shop'}>
+                <h3 className={styles.home_buttons}>
+                  View Collection
+                  <BiRightArrowAlt/>
+                </h3>
+              </Link>
+          </div>
+      </div>
+      </ImageSlider>
     </PageWrapper>
   )
 }
