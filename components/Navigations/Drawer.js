8eb621348b133916cc1e6c52882b9b4cea7699ca
MayurDeopa <frostkickerzvashi@gmail.com>
2022-05-12 12:47:02 +0530

    css

--- components/Navigations/Drawer.js
+++ components/Navigations/Drawer.js
@@ -1,38 +1,38 @@
 import { useState } from 'react';
 import { shop ,company,user} from '../../lib/drawer/draweroptions';
 import styles from '../../styles/Header.module.css'
-import NestedOptions from '../NestedComponent/NestedOptions';
+
 import FileStructure from '../NestedComponent/FileStructure';
-import Link from 'next/link'
 
 const Drawer =({state})=>{
     const [isHidden,setIsHidden] = state.drawerState
     return(
-        <nav className={isHidden?`${styles.drawer_wrapper} ${styles.drawer_hidden}`:styles.drawer_wrapper}>
-            <div className={styles.drawer}>
-                <FileStructure
-                    states={{
-                        title:"Shop",
-                        children:shop
-                    }}
-                />
-                <FileStructure
-                    states={{
-                        title:'Company',
-                        children:company
-                    }}
-                />
-                <FileStructure
-                    states={{
-                        title:'User',
-                        children:user
-                    }}
-                />
-            </div>  
-            <div 
-                className={isHidden?`${styles.drawer_container_hidden}`:styles.drawer_container} 
-                onClick={()=>setIsHidden(true)}/>
-        </nav>
+        <div 
+            className={isHidden?`${styles.drawer_container} ${styles.drawer_container_hidden}`:styles.drawer_container} 
+            onClick={()=>setIsHidden(true)}>
+            <nav className={isHidden?`${styles.drawer_wrapper} ${styles.drawer_hidden}`:styles.drawer_wrapper}>
+                <div className={styles.drawer}>
+                    <FileStructure
+                        states={{
+                            title:"Shop",
+                            children:shop
+                        }}
+                    />
+                    <FileStructure
+                        states={{
+                            title:'Company',
+                            children:company
+                        }}
+                    />
+                    <FileStructure
+                        states={{
+                            title:'User',
+                            children:user
+                        }}
+                    />
+                </div>              
+            </nav>
+        </div>
     )
 }
 
