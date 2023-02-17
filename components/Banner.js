523b96069b2a2320c81905241e3ecca12a47bdd7
MayurDeopa <frostkickerzvashi@gmail.com>
2023-02-17 20:41:22 +0530

    soon

--- components/Banner.js
+++ components/Banner.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import AntiMatterLogo from './Display/AntiMatterLogo';
 import SubHeading from './Typography/SubHeading'
 
 const Banner =()=>{
     return (
         <div className="global_banner">
+            <Head>
+                <title>AntiMatter | Coming Soon !</title>
+            </Head>
             <SubHeading>Dropping Soon !! </SubHeading>
         </div>
     )
