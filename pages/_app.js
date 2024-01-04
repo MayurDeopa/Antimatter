d2ca198e7956d43d3b83626a46f427196d2e386d
MayurDeopa <91918025+MayurDeopa@users.noreply.github.com>
2024-01-04 13:53:13 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -51,9 +51,9 @@ function MyApp({ Component, pageProps }) {
 
   const hasTranstioned = useTransition(path,1000)
 
-  return(
-    <Banner/>
-  )
+  // return(
+  //   <Banner/>
+  // )
 
   return (
     <QueryClientProvider client={queryClient}>
